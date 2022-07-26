7c4f02ba6650716f5e185551d3346c4243b9f011
notgaelramirez <gael1423@gmail.com>
2022-07-26 00:15:49 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import HomeScreen from './components/home/HomeScreen'
+
+function App() {
+
+  return (
+    <div className="App">
+      <HomeScreen />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/home/HomeScreen.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './home.css'
+import gael from '/src/assets/gael.png'
+
+const HomeScreen = () => {
+  return (
+    <div className='home'>
+      <div className='home-black'></div>
+
+      <img className='home-title' src={gael} alt="" />
+      <p className='home-subtitle'>
+        Frontend Developer
+      </p>
+
+      <div className="home-buttons">
+        <button className='home-cv-btn'>
+          Download CV
+        </button>
+
+        <button className='home-am-btn'>
+          About Me
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default HomeScreen
